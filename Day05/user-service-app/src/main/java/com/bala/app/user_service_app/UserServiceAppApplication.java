9b98c515bd9ba87cf9799ae45b31905aed10825e
Balamurugan <Bala@gmail.com>
2025-01-26 23:44:22 -0800

    Day05

---
+++ Day05/user-service-app/src/main/java/com/bala/app/user_service_app/UserServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.bala.app.user_service_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceAppApplication.class, args);
+	}
+
+}
