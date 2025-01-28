e7f25fdf5c4113e64ed492f29072ba2f1f0a6384
Balamurugan <Bala@gmail.com>
2025-01-27 22:47:13 -0800

    Day06 Spring

--- Day06/spring-boot-app/src/main/java/com/bala/app/App.java
+++ Day06/spring-boot-app/src/main/java/com/bala/app/App.java
@@ -11,7 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        //System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
         SpringApplication.run(App.class, args);
     }
 }
