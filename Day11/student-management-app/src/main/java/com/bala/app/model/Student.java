9022c0b7a7035d97c3353911d77bfbb303e03ca1
admin <rama@gail.com>
2025-02-04 01:33:25 -0800

    Day11

---
+++ Day11/student-management-app/src/main/java/com/bala/app/model/Student.java
@@ -0,0 +1,36 @@
+package com.bala.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@Table(name ="student")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(unique = true,nullable = false)
+    private String schoolName;
+    @Column(unique = true,nullable = true)
+
+
+}
