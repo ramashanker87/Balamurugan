30b935ef355816676fd039d7f4feb2094cf5e62a
admin <rama@gail.com>
2025-02-05 05:27:24 -0800

    Latest Task

--- Day11/student-management-app/src/main/java/com/bala/app/controller/StudentController.java
+++ Day11/student-management-app/src/main/java/com/bala/app/controller/StudentController.java
@@ -1,14 +1,26 @@
 package com.bala.app.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bala.app.model.Student;
+import com.bala.app.repository.StudentRepository;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
-private final StudentController studentController;
-public StudentController(StudentController studentController) {
-    this.studentController = studentController;
+private final StudentRepository studentRepository;
+public StudentController(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+}
+@GetMapping("/read")
+    public Iterable<Student> read() {
+    return studentRepository.findAll();
+}
+@PostMapping("/save")
+public String save(@RequestBody final Student student) {
+    System.out.println("Saving Student: " + student);
+    studentRepository.save(student);
+    return "Saved";
+
 }
 
 }
