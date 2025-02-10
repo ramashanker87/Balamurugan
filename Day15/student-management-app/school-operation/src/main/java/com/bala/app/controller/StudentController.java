5678b56b2d98bfe72bf6abb9092747a948f263cf
admin <rama@gail.com>
2025-02-10 00:31:53 -0800

    Day15

---
+++ Day15/student-management-app/school-operation/src/main/java/com/bala/app/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.bala.app.controller;
+
+import com.bala.app.model.Student;
+import com.bala.app.repository.StudentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/school")
+public class StudentController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StudentController.class);
+private final StudentRepository studentRepository;
+public StudentController(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+}
+@PostMapping("/save")
+    public Student save(@RequestBody Student student) throws Exception {
+    LOGGER.info("save student {}", student);
+    return studentRepository.save(student);
+}
+@GetMapping("/read")
+    public Iterable<Student> readAll() throws InterruptedException {
+    LOGGER.info("Reading all students");
+    return studentRepository.findAll();
+}
+
+}
