143d067e47e070082b2d2604554506d45b4c6c18
admin <rama@gail.com>
2025-02-13 08:36:02 -0800

    Day 18 Assignment

---
+++ Day18/patients-management-app/doctor-sqs-app/src/main/java/com/bala/app/PatientsManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.bala.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientsManagementAppApplication.class, args);
+	}
+
+}
