177a7b1587a48f4fbc0030f3a6d19f98b1d4ba57
admin <rama@gail.com>
2025-02-17 11:24:24 -0800

    Vehicle Logs task

--- Day20/vehicle-app/src/main/java/com/bala/app/controller/VehicleController.java
+++ Day20/vehicle-app/src/main/java/com/bala/app/controller/VehicleController.java
@@ -17,13 +17,23 @@ public class VehicleController {
     private static final Logger logger = LoggerFactory.getLogger(VehicleController.class);
     @Autowired
     private RegistrationService registrationService;
-    @PostMapping("/vehicle")
-    public String registerVehicle(@RequestBody Vehicle vehicle, @RequestBody Owner owner) {
-        Registration registration = new Registration(owner, vehicle);
-        registrationService.registerVehicle(vehicle.getVehicleNumber(),registration);
-        logger.info("Vehicle registered" + vehicle);
-        return "Vehicle registered successfully!";
-    }
+//    @PostMapping("/vehicle")
+//    public String registerVehicle(@RequestBody Vehicle vehicle, @RequestBody Owner owner) {
+//        Registration registration = new Registration(owner, vehicle);
+//        registrationService.registerVehicle(vehicle.getVehicleNumber(),registration);
+//        logger.info("Vehicle registered" + vehicle);
+//        return "Vehicle registered successfully!";
+//    }
+@PostMapping("/vehicle")
+public String registerVehicle(@RequestBody Registration registration) {
+    registrationService.registerVehicle(registration.getVehicle().getVehicleNumber(), registration);
+
+    // Log at info level for Vehicle registration
+    logger.info("Vehicle registered: " + registration.getVehicle());
+
+    return "Vehicle registered successfully!";
+}
+
     @DeleteMapping("/vehicle")
     public String deregisterVehicle(@RequestParam String vehicleNumber) {
         registrationService.deregisterVehicle(vehicleNumber);
